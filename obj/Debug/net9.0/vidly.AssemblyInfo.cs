dc419778cacd753e740d123b475c33b2b74ba17c
Jeeshan Ahamed <151646448+jeeshu123@users.noreply.github.com>
2024-11-17 23:40:23 +0530

    update code

--- obj/Debug/net9.0/vidly.AssemblyInfo.cs
+++ obj/Debug/net9.0/vidly.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vidly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed2cb8e3e604e01430813543b48dd2cb7b1d8ac6")]
 [assembly: System.Reflection.AssemblyProductAttribute("vidly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vidly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
